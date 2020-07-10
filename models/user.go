9d3a4e6ea76f06b6eff0b16ccf0ca2c2f704359c
Jonathan <jonathansunata@gmail.com>
2020-07-10 17:25:29 +0700

    commit

--- models/user.go
+++ models/user.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id, omitempty"`
-	FName string             `json:"first_name, omitempty" bson:"first_name, omitempty"`
-	LName string             `json:"last_name, omitempty" bson:"last_name, omitempty"`
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FName string             `json:"first_name,omitempty" bson:"first_name,omitempty"`
+	LName string             `json:"last_name,omitempty" bson:"last_name,omitempty"`
 }
