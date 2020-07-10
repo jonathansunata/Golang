2168e5522e8c6e9c5b8d2512fcd01a73a41f9398
Jonathan <jonathansunata@gmail.com>
2020-07-10 20:03:26 +0700

    commit

--- controllers/user.go
+++ controllers/user.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/jonathansunata/Golang/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type UserController struct {
@@ -45,13 +46,48 @@ func (uc *UserController) CreateUser(w http.ResponseWriter, req *http.Request) {
 
 	json.NewDecoder(req.Body).Decode(&u)
 
+	fmt.Println("%+v", u)
 	collection := uc.client.Database("golangapideveloper").Collection("user")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	result, _ := collection.InsertOne(ctx, u)
+	result, err := collection.InsertOne(ctx, u)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	w.WriteHeader(http.StatusCreated)
 
 	json.NewEncoder(w).Encode(result)
+}
+
+func (uc *UserController) GetUsers(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	collection := uc.client.Database("golangapideveloper").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+
+	var users []models.User
+	for cursor.Next(ctx) {
+		var u models.User
+
+		err = cursor.Decode(&u)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		users = append(users, u)
+	}
+
+	if err = cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(users)
 }
